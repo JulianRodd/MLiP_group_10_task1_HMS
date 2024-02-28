c06b5af6be5a7158819db9a38c6430cb081b3577
tabrom <tabrom@web.de>
2024-02-28 14:43:09 +0100

    failure analysis with test submission implemented

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-from utils.failure_utils import run_failure_analysis
-from datasets import data_loader_configs
-from models import custom_model_configs, CustomModel
-
+# from utils.failure_utils import run_failure_analysis
+# from datasets import data_loader_configs
+# from models import custom_model_configs, CustomModel
+from utils.failure_utils import majority_confusion, avg_mispred, plot_avg_mispred
+from generics import Generics, Paths
+import numpy as np
 
 
 
 def main():
-    model_config = custom_model_configs.EfficientNetB0ConfigV1
-    model=CustomModel.CustomModel(model_config)
+    y_pred = np.array([[1,0.1,2,0.1, 0.1, 0.1], [1,0.1,2,0.1, 0.1, 0], [1,0.1,0.1,5, 0.1, 0.1], [1,2,0.1,0.1, 0.1, 0.1], [1,0.1,0.1,3, 0.1, 5], [1,0.1,0.1,3, 0.1, 5]])
+    y_true = np.array([[1,0.1,0.1,0.1, 0.1, 0.1], [0.1,2,0.1,0.1,1, 0.1], [1,2,3,0.1, 0.1, 0.1], [1,0.1,2,4, 0.1, 0], [1,0.1,2,0.1, 5, 0], [1,0.1,2,0.1, 5, 6]])
+    # print(majority_confusion(y_pred, y_true, Generics.LABEL_COLS))
 
-    run_failure_analysis(model_config=model_config,
-        data_loader_config=data_loader_configs.SmallBaseConfig, model=model)
+    misp = avg_mispred(y_pred, y_true, by_class=True)
+    print(misp)
+    plot_avg_mispred(misp, by_class=True)
+
+    # model_config = custom_model_configs.EfficientNetB0ConfigV1
+    # model=CustomModel.CustomModel(model_config)
+
+    # run_failure_analysis(model_config=model_config,
+    #     data_loader_config=data_loader_configs.SmallBaseConfig, model=model)
     
     
 
